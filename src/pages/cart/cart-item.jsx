723949b8231e75902109fb64d6336a68ebb09047
olexandervanzuriak <sashavanzuriak@gmail.com>
2023-11-08 21:14:25 +0200

    fix small problem with changing items amount

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -16,7 +16,7 @@ export const CartItem = (props) => {
             <p>$ {price}</p>
             <div className="countHandler">
                 <button onClick={() => removeFromCart(id)}> - </button>
-                <input value={cartItems[id]} onChange={(e) => updateCartItemCount(e.target.value, id)} />
+                <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
                 <button onClick={() => addToCart(id)}> + </button>
             </div>
         </div>
